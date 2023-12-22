64803638d116a4b881e67da550df62db4abd280f
tomhosker <tomdothosker@gmail.com>
2023-12-22 21:27:20 +0000

    Added manifest.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 # Local constants.
 PACKAGE_NAME = "hpml"
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 DESCRIPTION = "Hosker's Poetical Markup Language"
 GIT_URL_STEM = "https://github.com/tomhosker"
 AUTHOR = "Tom Hosker"
